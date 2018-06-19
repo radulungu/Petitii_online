13c4cff5d940679a26d198ebfdef5c37df84a941
radulungu <radu_lungu97@yahoo.com>
2018-06-19 23:46:39 +0300

    Add files via upload

---
+++ Controllers/HelperClasses/connection.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 10-Apr-17
+ * Time: 17:45
+ */
+class OracleConnection
+{
+
+    private  const USERNAME = 'PETITII';
+    private  const PASSWORD = 'PETITII';
+    private  const CONNECTION_STRING = 'localhost/xe';
+    private static $connection;
+
+    public static function getConnection()
+    {
+        if (!self::$connection) {
+            self::$connection = oci_connect(self::USERNAME, self::PASSWORD, self::CONNECTION_STRING);
+            if (!self::$connection)
+            {
+                echo "Connection error!";
+                return null;
+            }
+        }
+
+        return self::$connection;
+    }
+
+    public static function closeConnection()
+    {
+
+        oci_close(self::$connection);
+    }
+}
+
+?>
